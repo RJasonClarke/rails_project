67f17ee4bd5cdc9e5b54b81ff82d89cf2b9feb92
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2021-08-01 16:31:18 -0500

    working on user controller

--- rails-p/app/models/user.rb
+++ rails-p/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :lists
     has_many :movies
     has_many :movie_lists, through: :movies, source: :list
--- rails-p/config/routes.rb
+++ rails-p/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get '/register', to: 'users#new'
+  post '/register', to: 'users#create'
+
   resources :categories
   resources :movies
   resources :users
